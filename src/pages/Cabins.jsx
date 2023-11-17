99a437a6b89dbbee7171364bfd4da6fe3b9ed477
jagpratap <jagpratapchauhan@gmail.com>
2023-11-17 23:40:56 +0530

    Setup cabin table with data fetching

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,18 +1,20 @@
-import { useEffect } from "react";
-import { getCabins } from "../services/apiCabins";
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  useEffect(() => {
-    getCabins().then((data) => console.log(data));
-  }, []);
-
   return (
-    <div>
-      <img
-        src="https://jtvuscpjmcbebkkbcwvp.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
-        alt=""
-      />
-    </div>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
