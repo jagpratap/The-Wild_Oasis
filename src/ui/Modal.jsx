ef9af46af6d6e6bf65e028874c8bcdde332ba807
jagpratap <jagpratapchauhan@gmail.com>
2023-12-03 17:45:28 +0530

    Improve modal functionality with outside click closing

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import { cloneElement, createContext, useContext, useState } from "react";
 import { HiXMark } from "react-icons/hi2";
 
+import { useOutsideClick } from "../hooks/useOutsideClick";
+
 const StyledModal = styled.div`
   position: fixed;
   top: 50%;
@@ -75,12 +77,13 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
+  const ref = useOutsideClick(close);
 
   if (name !== openName) return null;
 
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
